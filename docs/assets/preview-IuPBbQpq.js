29fa22afcb25a33664dad759a1c6034f5017e340
nmarino <nicodemo.marino74@gmail.com>
2024-01-08 19:16:22 +0100

    Storybook listo

---
+++ docs/assets/preview-IuPBbQpq.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-CrWsEo2V.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-5iH0LfNu.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-5iH0LfNu.js","./iframe-CrWsEo2V.js","./index-XiNr8FW2.js","./_commonjsHelpers-5-cIlDoe.js","./react-18-sFOusbzT.js","./index-9vG4XYWr.js","./index-6FLSpKcG.js","./index-ogXoivrg.js","./index-mLPG47JP.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
